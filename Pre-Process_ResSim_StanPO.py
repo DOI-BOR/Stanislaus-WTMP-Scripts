2d6e5f516331742a467e2515719b0773a418e1fc
Nuffer-Rodriguez, Frankie L <fnufferrodriguez@usbr.gov>
2025-12-05 09:26:56 -0700

    added search and removal of old paths before appending current

--- Pre-Process_ResSim_StanPO.py
+++ Pre-Process_ResSim_StanPO.py
@@ -15,6 +15,29 @@ import os, sys
 from com.rma.io import DssFileManagerImpl
 from com.rma.model import Project
 #import hec.hecmath.TimeSeriesMath as tsmath
+# print current path
+print("Current paths: ", sys.path)
+
+# create list of unwanted folders in sys.path
+search_list = ["SacTrn", "Sacramento", "American", "Stanislaus"]
+
+# initialize and search for unwanted paths
+matching_paths = []
+for p in sys.path:
+    if any(phrase in p for phrase in search_list):
+        matching_paths.append(p)
+
+# print paths containing unwanted phrases
+print("Paths to be removed:")
+for path in matching_paths:
+    print(path)
+
+# remove matching paths from sys.path
+for path in matching_paths:
+    if path in sys.path:
+        sys.path.remove(path)
+
+# append path
 sys.path.append(os.path.join(Project.getCurrentProject().getWorkspacePath(), "scripts"))
 
 
